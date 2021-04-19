8f5389d5cb580b53dd8423902114ec0626ee7900
jisphert <pourt.it@gmail.com>
2021-04-20 03:56:57 +0800

    Refactor BlogController

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -3,48 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\AddCommentRequest;
+use App\Http\Requests\AddReplyRequest;
 use App\Http\Services\Blog\CommentService;
 use App\Http\Services\Blog\PostService;
+use App\Models\Comment;
 use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
-    public function __construct()
-    {
-        
-    }
-
-    public function index()
+    public function addComments(AddCommentRequest $request)
     {
         try {
             
-            $posts = (new PostService)->getPosts();
-
-        } catch (\Exception $e) {
-            return response(
-                [
-                    'message' => $e->getMessage(),
-                    'code' => $e->getCode()
-                ], 
-                422
-            );
-        }
-
-        return response(
-            [
-                'posts' => $posts,
-                'error' => false,
-                'message' => 'Successfully retrieved posts'
-            ],
-            200
-        );
-    }
-
-    public function show($id)
-    {
-        try {
-            
-            $post = (new PostService)->getPostById($id);
+            $post = (new CommentService)->addComment($request);
 
         } catch (\Exception $e) {
             return response(
@@ -60,43 +31,17 @@ class BlogController extends Controller
             [
                 'post' => $post,
                 'error' => false,
-                'message' => 'Successfully retrieved post'
+                'message' => 'Comment successfully added'
             ],
-            200
+            201
         );
     }
 
-    public function addComments(AddCommentRequest $request, int $postId)
+    public function getComments(int $commentId = 0)
     {
         try {
             
-            $post = (new CommentService)->addComment($request, $postId);
-
-        } catch (\Exception $e) {
-            return response(
-                [
-                    'message' => $e->getMessage(),
-                    'code' => $e->getCode()
-                ], 
-                422
-            );
-        }
-
-        return response(
-            [
-                'post' => $post,
-                'error' => false,
-                'message' => 'Successfully commented post'
-            ],
-            200
-        );
-    }
-
-    public function getComments(int $postId)
-    {
-        try {
-            
-            $comments = (new CommentService)->getComments($postId);
+            $comments = (new CommentService)->getComments($commentId);
 
         } catch (\Exception $e) {
             return response(
@@ -112,7 +57,7 @@ class BlogController extends Controller
             [
                 'comments' => $comments,
                 'error' => false,
-                'message' => 'Successfully commented post'
+                'message' => 'Comments successfully retrieved'
             ],
             200
         );
