ebe36a16410ec455de854f3bf6243eaa2ca21fce
jisphert-hs <pourt.it@gmail.com>
2021-04-20 22:36:13 +0800

    Refactor CommentCollection

--- app/Http/Resources/CommentCollection.php
+++ app/Http/Resources/CommentCollection.php
@@ -15,6 +15,56 @@ class CommentCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return $this->collection;
+        $commentCollection = $this->collection;
+        $commentObject = [];
+
+        foreach ($commentCollection as $comment) 
+        {
+            $replyObject = [];
+
+            if ($comment->replies->count()) 
+            {
+                foreach($comment->replies as $reply)
+                {
+                    $nestedReplyObject = [];
+
+                    if ($reply->replies->count())
+                    {
+                        foreach($reply->replies as $nestedReply)
+                        {
+                            $nestedReplyObject[] = $nestedReply;
+                        } 
+
+                        $replyObject[] = [
+                            "id" => $reply->id,
+                            "name" => $reply->name,
+                            "comment" => $reply->comment,
+                            "created_at" => $reply->created_at,
+                            "replies" => $nestedReplyObject
+                        ];
+
+                    } else {
+                        $replyObject[] = $reply;
+                    }
+
+                }
+
+            }
+
+            if ($comment->comment_id == null) 
+            {
+                $commentObject[] = [
+                    "id" => $comment->id,
+                    "name" => $comment->name,
+                    "comment" => $comment->comment,
+                    "created_at" => $comment->created_at,
+                    "replies" => $replyObject
+                ];
+            }
+
+        }
+
+        return $commentObject;
     }
+
 }
