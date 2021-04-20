0841c955d06fd01439f860e028cf761a410e99c5
jisphert-hs <pourt.it@gmail.com>
2021-04-20 22:38:00 +0800

    Refactor Comments component

--- resources/js/components/blog/Comments.vue
+++ resources/js/components/blog/Comments.vue
@@ -6,63 +6,77 @@
         <div class="">           
             <div v-for="comment in comments" :key="comment.id" class="">
                 <div class="d-flex mb-3">
-                    <div class="border w-100">
+                    <div class="w-100">
                         <div class="p-2">
                             <b><big>{{comment.name}}</big></b> 
                             <small><small>{{comment.created_at}}</small></small>
                         </div>
-                        <div class="pl-5 pt-3 pb-3 pr-3">
+                        <div class="pl-2 pt-1 pr-3">
                             <div class="bg-light">
                                 {{comment.comment}}
                             </div>
+                            <a class="btn btn-link" @click="openReplyForm(comment)" >Reply</a>
                         </div>
-                        <div class="d-flex flex-row justify-content-between p-0">
-                            <div class="flex-fill p-0">
-                                <a class="btn btn-secondary rounded-0 w-100" @click="openReplyForm(comment)" v-show="openCommentId !== comment.id">Leave a Reply</a>
+                        <div v-show="openCommentId === comment.id"  class="border-1">
+                            <div class="">
+                                <div class="text-right">
+                                    <a class="btn btn-link"  @click="closeReplyForm()">Close</a>
+                                </div>
+                                <input v-model="reply_name"  class="form-control rounded-0" type="text" placeholder="Name" />
+                            </div>
+                            <div class="">
+                                <textarea v-model="reply_message" class="form-control rounded-0" placeholder="Reply" ></textarea>
+                            </div>
+                            <div class="">
+                                <input @click="addComment(comment)" class="btn w-100 btn-primary rounded-0" type="button" value="Reply" />
                             </div>
                         </div>
-                        <div v-for="reply in comment.replies" :key="reply.id" class="">
+
+                        <!-- REPLY -->
+                        <div v-for="reply in comment.replies" :key="reply.id" class="ml-5 mt-5 border-1">
                             <div class="p-2">
                                 <b><big>{{reply.name}}</big></b> 
                                 <small><small>{{reply.created_at}}</small></small>
                             </div>
-                            <div class="pl-5 pt-3 pb-3 pr-3">
+                            <div class="pl-2 pt-1 pb-3 pr-3">
                                 <div class="bg-light">
                                     {{reply.comment}}
                                 </div>
+                                <a class="btn btn-link" @click="openReplyForm(reply)" >Reply</a>
                             </div>
-                            <div class="d-flex flex-row justify-content-between p-0">
-                                <div class="flex-fill p-0">
-                                    <a class="btn btn-secondary rounded-0 w-100" @click="openReplyForm(reply)" v-show="openCommentId !== reply.id">Leave a Reply</a>
+                            <div v-show="openCommentId === reply.id">
+                                <div class="">
+                                    <div class="text-right">
+                                        <a class="btn btn-link"  @click="closeReplyForm()">Close</a>
+                                    </div>
+                                    <input v-model="reply_name"  class="form-control rounded-0" type="text" placeholder="Name" />
+                                </div>
+                                <div class="">
+                                    <textarea v-model="reply_message" class="form-control rounded-0" placeholder="Reply" ></textarea>
+                                </div>
+                                <div class="">
+                                    <input @click="addComment(reply)" class="btn w-100 btn-primary rounded-0" type="button" value="Reply" />
                                 </div>
                             </div>
-                            <div class="">
-                                <div v-show="openCommentId === comment.id">
-                                    <div class="">
-                                        <input v-model="reply.name"  class="form-control rounded-0" type="text" placeholder="Name" />
-                                    </div>
-                                    <div class="">
-                                        <textarea v-model="reply.message" class="form-control rounded-0" placeholder="Reply" ></textarea>
-                                    </div>
-                                    <div class="">
-                                        <input @click="addComment(comment)" class="btn w-100 btn-primary rounded-0" type="button" value="Reply" />
+
+                            <!-- NESTED REPLY -->
+                            <div v-for="nestedReply in reply.replies" :key="nestedReply.id" class="ml-5 mt-5 border-1">
+                                <div class="p-2">
+                                    <b><big>{{nestedReply.name}}</big></b> 
+                                    <small><small>{{nestedReply.created_at}}</small></small>
+                                </div>
+                                <div class="pl-2 pt-1 pb-3 pr-3">
+                                    <div class="bg-light">
+                                        {{nestedReply.comment}}
                                     </div>
                                 </div>
                             </div>
+                            
+                            <!-- END NESTED REPLY -->
+                            
                         </div>
-                        <div class="">
-                            <div v-show="openCommentId === comment.id">
-                                <div class="">
-                                    <input v-model="reply.name"  class="form-control rounded-0" type="text" placeholder="Name" />
-                                </div>
-                                <div class="">
-                                    <textarea v-model="reply.message" class="form-control rounded-0" placeholder="Reply" ></textarea>
-                                </div>
-                                <div class="">
-                                    <input @click="addComment(comment)" class="btn w-100 btn-primary rounded-0" type="button" value="Reply" />
-                                </div>
-                            </div>
-                        </div>
+                        <!-- END REPLY -->
+
                     </div>
                 </div>
             </div>
@@ -85,25 +99,17 @@
 
 <script>
   export default {
-    props: {
-        comments: {
-            type: Object,
-            required: true
-        }
-    },
     data () {
       return {
         name: null,
         message: null,
+        reply_name: null,
+        reply_message: null,
         openCommentId: null,
         comments: [],
         comment: {
             name: null,
             message: null,
-        },
-        reply: {
-            name: null,
-            comment: null
         }
       }
     },
@@ -125,19 +131,21 @@
             if (comment_id) {
                 data = {
                     comment_id: comment_id,
-                    name: this.reply.name,
-                    comment: this.reply.message,
+                    name: this.reply_name,
+                    comment: this.reply_message
                 };
             }
+
+            console.log('data', data);
 
             axios.post(`/api/comments`, data)
                 .then((response) => {
 
-                    this.getComments();
-
                     this.resetForm();
 
                     this.closeReplyForm();
+
+                    this.getComments();
                 })
         },
 
@@ -151,10 +159,12 @@
         },
 
         resetForm () {
-            this.message = this.name = this.reply.name = this.reply.message = "";
+            this.message = this.name = this.reply_name = this.reply_message = "";
         },
 
         openReplyForm (comment) {
+            this.openCommentId = null;
+
             this.openCommentId = comment.id;
         },
 
